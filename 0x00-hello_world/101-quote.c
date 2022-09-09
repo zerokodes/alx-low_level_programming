888408b89fb28244e4095f55dd69a510112b30e0
zerokodes <henrysam140@gmail.com>
2022-09-08 18:15:35 -0700

    modified file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/**
- * main - Entry point
+
+/** main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-  printf("and that piece of art is useful\" - Dora \ Korpar, 2015-10-19\n");
-  return (1);
+ fprintf(stderr, "and that piece of art is useful\" - Dora \
+		 Korpar, 2015-10-19\n");
+ return (1);
 }
