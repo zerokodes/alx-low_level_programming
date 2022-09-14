05c34728b30a526d32ab76d04d1028b11c65c5d2
zerokodes <henrysam140@gmail.com>
2022-09-13 18:09:52 -0700

    fixed bug

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,13 +9,17 @@ void jack_bauer(void)
 	int i, j;
 
 	i = 0;
-	for (; i < 60 ; i++)
+	for (; i < 024; i++)
 	{
+		j = 0;
+		for (; j < 60; j++)
+		{
 		_putchar((i / 10) + '0');
 		_putchar((i % 10) + '0');
 		_putchar(':');
 		_putchar((j / 10) + '0');
 		_putchar((j % 10) + '0');
 		_putchar('\n');
+		}
 	}
 }
