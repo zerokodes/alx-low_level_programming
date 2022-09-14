79e62af7eaf7b0a2d860668dabf41af1ceb2d46e
zerokodes <henrysam140@gmail.com>
2022-09-13 18:43:46 -0700

    fixed bug

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,28 +9,28 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-	for (j = 0; j < 10; j++)
-	{
-		k = j * i;
-		if (j == 0)
+		for (j = 0; j < 10; j++)
 		{
-			_putchar(k + '0');
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
-		if (k < 10 && j != 0)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(k + '0');
-		}
-		else if (k >= 10)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar((k / 10) + '0');
-			_putchar((k % 10) + '0');
-		}
-	}
 	}
 	_putchar('\n');
 }
