4c1cd7293d2ae0d7307233e5bd82b84643e1027e
zerokodes <henrysam140@gmail.com>
2022-09-15 13:39:16 -0700

    print small letters

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - Printing small letters
+ * @c: a number
+ * Return: whether the number is between 0 and 9
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (0);
+	else
+		return (0);
+}
