4a10aba6504d3e959f0e6ab0fb5ffcbbefc15ca9
zerokodes <henrysam140@gmail.com>
2022-09-15 13:12:13 -0700

    checks if character is lowercase

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _isupper(int c);
+int is_digit(int);
+int mul(int, int);
+void print_numbers(void);
+int _putchar(char);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+
+#endif
