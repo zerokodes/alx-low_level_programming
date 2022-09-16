0a38df4a8d2dbee9a8b4d337b2b3d80021144fca
zerokodes <henrysam140@gmail.com>
2022-09-15 17:08:14 -0700

    finds and prints the largest prime factor of the number 612852475143

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_triangle - print triangles using '#'
+ * @size: the size of the triangle
+ * Return: void
+ */
+void print_triangle(int size)
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (k = size - 1 - i; k > 0; k--)
+		{
+			_putchar(' ');
+		}
+		for (j = 0; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		if (size <= 0)
+		{
+			_putchar('\n');
+		}
+	}
