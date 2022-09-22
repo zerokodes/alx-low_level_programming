2a4098f8a2a52968108b323c4795c90faae99e21
zerokodes <henrysam140@gmail.com>
2022-09-22 12:47:30 -0700

    Write a function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcat  - concatenate two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: Always 0
+ */
+char _strcat(char s1[], char s2[])
+{
+	strcat(s1, s2);
+	return (0);
+}
