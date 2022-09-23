9f600fe8975e68706fc8ef0a581ebda5e2dcd3f0
zerokodes <henrysam140@gmail.com>
2022-09-22 17:06:28 -0700

    Add one line to this code, so that the program prints a[2] = 98, followed by a new line.

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,13 +7,14 @@ int main(void)
 	int *p;
 
 	a[2] = 1024;
-	/**
+	p = &n;
+	/*
 	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
-	 *   - only one statemen
-	 *   - you are not allowed to code anything else than this line of code
+	 * - only one statemen
+	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
