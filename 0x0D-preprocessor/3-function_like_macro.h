74efd35eb175e097860b7219a0839c8f37681f7b
zerokodes <nduka.henrychisom@gmail.com>
2022-10-10 17:19:59 -0700

    modified file

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef ABS_H
-#define ABS_H
-#define ABS(x) (((x) < 0) ? -(x) : (x))
-#endif /* ABS_H *i
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
+#define ABS(x) (((x) < 0) ? (x * -1) : (x))
+#endif
