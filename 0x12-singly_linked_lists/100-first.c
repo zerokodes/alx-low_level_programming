b412abe6ac6b44e519ff8c8ac4eaae8952c87df6
zerokodes <henrysam140@gmail.com>
2022-10-20 17:21:28 -0700

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ *myStartupFun - start before main.
+ *@void: input.
+ *Return: text.
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
