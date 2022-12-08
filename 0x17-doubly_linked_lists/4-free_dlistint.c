c180e6e2f86b23d420b9cf339c6d7bfcb00b6e01
Nduka Henry Chisom <henrysam140@gmail.com>
2022-12-09 00:17:07 +0100

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,3 +1,17 @@
-iirhftcbbd
-ddvdhhsds
-sdjskkjnk
+#include "lists.h"
+
+/**
+ *free_dlistint - Frees the linked dlistint list
+ *@head: The head of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
