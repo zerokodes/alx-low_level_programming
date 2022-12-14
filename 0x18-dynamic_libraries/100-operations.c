aecd5189b29aaf0f3d2220073c7c35db2d1e4449
Nduka Henry Chisom <henrysam140@gmail.com>
2022-12-14 11:43:23 +0000

    fixed bug

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,50 +1,50 @@
 #include "operations.h"
 /**
-* add - add two numbers
-* @a: first number
-* @b: second number
-* Return: 1
-*/
+ *add - add two numbers
+ *@a: first number
+ *@b: second number
+ *Return: 1
+ */
 int add(int a, int b) 
 { 
     return (a + b); 
 }
 /**
-* sub - subtract two numbers
-* @a: first number
-* @b: second number
-* Return: 1
-*/
+ *sub - subtract two numbers
+ *@a: first number
+ *@b: second number
+ *Return: 1
+ */
 int sub(int a, int b) 
 { 
     return (a - b); 
 }
 /**
-* mul - multiply two numbers
-* @a: first number
-* @b: second number
-* Return: 1
-*/ 
+ *mul - multiply two numbers
+ *@a: first number
+ *@b: second number
+ *Return: 1
+ */ 
 int mul(int a, int b) 
 { 
     return (a * b); 
 }
 /**
-* div - divide two numbers
-* @a: first number
-* @b: second number
-* Return: 1
-*/ 
+ *div - divide two numbers
+ *@a: first number
+ *@b: second number
+ *Return: 1
+ */ 
 int div(int a, int b) 
 { 
     return (a / b); 
 }
 /**
-* mod - remainder from division of two numbers
-* @a: first number
-* @b: second number
-* Return: 1
-*/ 
+ *mod - remainder from division of two numbers
+ *@a: first number
+ *@b: second number
+ *Return: 1
+ */ 
 int mod(int a, int b) 
 { 
     return (a % b); 
