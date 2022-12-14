97a49f2dd9a89b4df513f94455104e69cf6146b1
Nduka Henry Chisom <henrysam140@gmail.com>
2022-12-14 11:17:23 +0000

    fixed bug

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,4 +1,13 @@
 #include "operations.h"
+/**
+* add - add two numbers
+* sub - subtract two numbers
+* mul - multiply two numbers
+* div - divide two numbers
+* mod - remainder from the divivsion of two numbers
+* @a: first number
+* @b: second number
+*/
 
 int add(int a,int b) 
 { 
