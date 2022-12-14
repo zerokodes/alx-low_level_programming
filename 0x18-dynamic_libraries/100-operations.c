c6ffe1f58746fbe1af534b553af4d96f1256f61e
Nduka Henry Chisom <henrysam140@gmail.com>
2022-12-14 11:24:06 +0000

    fixed bug

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -3,6 +3,7 @@
 * add - add two numbers
 * @a: first number
 * @b: second number
+* Return: 1
 */
 int add(int a,int b) 
 { 
@@ -13,6 +14,7 @@ int add(int a,int b)
 * sub - subtract two numbers
 * @a: first number
 * @b: second number
+* Return: 1
 */
 int sub(int a, int b) 
 { 
@@ -23,6 +25,7 @@ int sub(int a, int b)
 * mul - multiply two numbers
 * @a: first number
 * @b: second number
+* Return: 1
 */ 
 int mul(int a, int b) 
 { 
@@ -33,6 +36,7 @@ int mul(int a, int b)
 * div - divide two numbers
 * @a: first number
 * @b: second number
+* Return: 1
 */ 
 int div(int a, int b) 
 { 
@@ -43,6 +47,7 @@ int div(int a, int b)
 * mod - remainder from division of two numbers
 * @a: first number
 * @b: second number
+* Return: 1
 */ 
 int mod(int a, int b) 
 { 
